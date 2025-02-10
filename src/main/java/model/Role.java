44d6d91f45f25504910cb4552613354022c0b9d7
Alex A <admin@example.com>
2025-02-10 11:57:20 +0300

    Новый проект Spring Boot Security

---
+++ src/main/java/model/Role.java
@@ -0,0 +1,27 @@
+package model;
+
+import jakarta.persistence.*;
+import org.springframework.security.core.GrantedAuthority;
+
+import java.util.Set;
+
+@Entity
+@Table(name = "roles")
+public class Role implements GrantedAuthority {
+
+    @Id
+    private Long id;
+
+    private String name;
+
+    @Transient
+    @ManyToMany(mappedBy = "roles")
+    private Set<User>
+            users;
+    public Role() {}
+
+    @Override
+    public String getAuthority() {
+        return "";
+    }
+}
---
+++ src/main/java/model/User.java
@@ -0,0 +1,83 @@
+package model;
+
+import jakarta.persistence.*;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
+import java.util.Set;
+
+@Entity
+@Table(name = "users")
+public class User implements UserDetails {
+
+    @Id
+    private Long id;
+
+    private String username;
+
+    private String password;
+
+    private String phone;
+
+    @ManyToMany(fetch = FetchType.LAZY)
+    private Set<Role> roles;
+
+    public User(){}
+
+    public User(Long id, String username, String password, String phone, Set<Role> roles) {
+        this.id = id;
+        this.username = username;
+        this.password = password;
+        this.phone = phone;
+        this.roles = roles;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public Set<Role> getRoles() {
+        return roles;
+    }
+
+    public void setRoles(Set<Role> roles) {
+        this.roles = roles;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return getRoles();
+    }
+
+    @Override
+    public String getPassword() {
+        return password;
+    }
+
+    @Override
+    public String getUsername() {
+        return username;
+    }
+}
